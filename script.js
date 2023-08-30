bebfaf25d7af9c22d26fcc061c7460c231057613
Patrick-Tomic <pat12tomic@gmail.com>
2023-08-30 17:15:35 -0400

    created function for decimal, delete, and some styling

--- script.js
+++ script.js
@@ -6,12 +6,49 @@ const functions = document.querySelectorAll("#function");
 const equal = document.querySelector("#equal");
 const numbers = document.querySelectorAll("#number");
 const screen = document.querySelector("#screen");
+const decimal = document.querySelector("#decimal");
+const backspace = document.querySelector("#backspace");
 let firstNumber=0;
 let sign = "";
 let secondNumber = 0;
+let int = true;
+let float = false;
 
+   
+function Delete(str){
+    let array = str.split("");
+    array.splice(array.length-1,array.length);
+    return array.join("");
+}
+
+    backspace.addEventListener("click",()=>{
+        if(sign!=""){
+           secondNumber=Delete(secondNumber);
+           
+            
+        }
+        else{
+            firstNumber=Delete(firstNumber);
+           
+        }
+    })
+    backspace.addEventListener("click",()=>{
+        if(sign!=""){
+          
+            screen.textContent=secondNumber;
+            
+        }
+        else{
+            
+            screen.textContent=firstNumber;
+        }
+    })
+    decimal.addEventListener("click",()=>{
+        int = false;
+        float = true;
+    })
     functions.forEach(func =>func.addEventListener("click",()=>{sign= func.textContent}));
-    
+ 
 
 function set(num){
     if(sign!=""){
@@ -135,22 +172,39 @@ function set(num){
             firstNumber = firstNumber+"9";
             break; 
         case 0: 
-        if(sign!=""){
-            if(secondNumber==0){secondNumber = "0";
+            if(sign!=""){
+                if(secondNumber==0){secondNumber = "0";
+                
+            break;}
+            secondNumber = secondNumber+"0";
             
-        break;}
-        secondNumber = secondNumber+"0";
-        
-        break;
+            break; 
         }
             if(firstNumber==0){firstNumber = "0";
             break;}
             firstNumber=firstNumber+"0";
             break;
+        case 10:
+            if(sign !=""){
+                if(secondNumber.indexOf(".")!=-1){
+                    break;
+                }
+                if(secondNumber ==0){secondNumber=".";
+                break;};
+                secondNumber = secondNumber+".";
+                break;
+            }
+            if(firstNumber.indexOf(".")!=-1){
+                break;
+            }
+            if(firstNumber ==0){firstNumber = ".";
+            break;}
+            firstNumber = firstNumber+".";
+            break;
+        
+            
     }
 } 
-
- 
 numbers.forEach(number=>number.addEventListener("click",()=>{
     if(sign!=""){
         screen.textContent=secondNumber;
@@ -164,16 +218,32 @@ numbers.forEach(number=>number.addEventListener("click",()=>{
     
     
 function subtract(first,second){
+    if(float ==true){
+        screen.textContent = parseFloat(first)-parseFloat(second);
+        return;
+    }
         screen.textContent=parseInt(first)-parseInt(second);
     }
 
 function addition(first,second){
+    if(float ==true){
+        screen.textContent = parseFloat(first)+parseFloat(second);
+        return;
+    }
    screen.textContent=parseInt(first)+parseInt(second);
 }
 function multiply(first,second){
+    if(float ==true){
+        screen.textContent = parseFloat(first)*parseFloat(second);
+        return;
+    }
     screen.textContent = parseInt(first)*parseInt(second);
 }
 function divide(first, second){
+    if(float ==true){
+        screen.textContent = parseFloat(first)/parseFloat(second);
+        return;
+    }
     screen.textContent = parseInt(first)/parseInt(second);
 }
 equal.addEventListener("click",()=>{
